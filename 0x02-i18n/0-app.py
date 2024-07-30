2a0b8e4d89681e8e071449948ce47ac6d3334fc5
francotaind <francotaind00@gmail.com>
2024-07-30 12:32:23 +0300

    minor correction

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,9 +1,17 @@
+#!/usr/bin/env python3
+"""Basic Flask app, index page"""
+
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
+    """Index
+    Returns:
+        str: HTML
+    """
     return render_template('index.html')
 
 if __name__ == '__main__':
