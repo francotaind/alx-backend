8a38b076ca7dc3b2f61a78bdb89888c3e99a428b
francotaind <francotaind00@gmail.com>
2024-10-31 08:55:36 +0300

    syntax fixes

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -6,6 +6,7 @@ from typing import List
 import csv
 import math
 
+
 def index_range(page: int, page_size: int) -> tuple:
     """
     Return a tuple containing start and end indexes for pagination
@@ -21,16 +22,17 @@ def index_range(page: int, page_size: int) -> tuple:
     end_index = start_index + page_size
     return (start_index, end_index)
 
+
 class Server:
     """
         server class to paginate of popular baby names
     """
-    DATA_FILE = "/home/francota/alx-backend/0x00-pagination/Popular_Baby_Names.csv"
+    DATA_FILE = "/francota/alx-backend/0x00-pagination/Popular_Baby_Names.csv"
 
     def __init__(self):
         """initialize the server"""
         self.__dataset = None
-        
+
     def dataset(self) -> List[List]:
         """cached dataset"""
         if self.__dataset is None:
@@ -50,7 +52,7 @@ class Server:
             page_size(int): The number of items per page
 
         Returrns:
-            List[list]: A list of rows from the dataset corresponding to the page
+            List[list]: A list of rows from the dataset  the page
         """
         assert isinstance(page, int) and page > 0
         assert isinstance(page_size, int) and page_size > 0
